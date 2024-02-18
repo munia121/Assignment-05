4c0c3b8680318e82af7c66897ed1b0cc2d561ab8
unknown <muniyaakter21@gmail.com>
2024-02-18 15:00:52 +0600

    added

--- script/index.js
+++ script/index.js
@@ -2,3 +2,31 @@ const scrollBtn = document.getElementById('scroll-Btn').addEventListener('click'
     const greenLineScroll = document.getElementById('greenLine-scroll');
     greenLineScroll.scrollIntoView({ behavior: 'smooth' });
 })
+
+
+
+// click button and change background color
+let count = 0;
+const selectedBtn = document.querySelectorAll('.seat-btn')
+for(let btn of selectedBtn){
+    btn.addEventListener('click', function(e){
+        e.target.style.backgroundColor = '#1DD100';
+        e.target.style.color = 'white'
+
+
+        count++;
+
+        const setCount = document.getElementById('set-count');
+        setCount.innerText = count;
+        
+
+        const seatsLeft = document.getElementById('seats-left').innerText;
+        const convertSeatsLeft = parseInt(seatsLeft) -1 ;
+        document.getElementById('seats-left').innerText = convertSeatsLeft;
+
+
+        
+        
+       
+    })
+}
